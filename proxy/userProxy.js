58820bbd2adcc3f6a907bf7df2b6adbdff2d2df7
a451114984 <451114984@qq.com>
2016-06-21 13:47:22 +0800

    modify

--- proxy/userProxy.js
+++ proxy/userProxy.js
@@ -74,7 +74,7 @@ exports.create = function(userInfo, callback){
       connection.beginTransaction((err)=>{
             if (err) { throw err; }
             connection.query('INSERT INTO user VALUES(:id, :passWords, :userName, :university, :faculty, :grade, :class, :picture)',
-              userInfo.toString();, (err, result)=>{
+              userInfo;, (err, result)=>{
               if (err) {
                   connection.rollback(function() {
                       throw DBError();
