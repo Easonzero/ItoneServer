8e28aeae364ae1856ed4deb3babcadf6dd50be72
a451114984 <451114984@qq.com>
2016-07-11 17:59:35 +0800

    modify

--- config.js
+++ config.js
@@ -2,19 +2,19 @@ function initConfig() {
     var configInfo = {
         default_max_conns : 50,
 
-        /*mysqlConfig       : {
+        mysqlConfig       : {
               "host"      : "127.0.0.1",
               "user"      : "root",
               "password"  : "wangyi601466673",
               "database"  : "itone"
-        },*/
+        },
 
-        mysqlConfig       : {
+        /*mysqlConfig       : {
            "host"      : "127.0.0.1",
             "user"      : "root",
             "password"  : "123",
             "database"  : "itone"
-        },
+        },*/
         
         sessionConfig     : {
             secret      : '123456',
